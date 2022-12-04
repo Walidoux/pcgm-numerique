1324e659d5129c214379274432f27d6b0eb0b6ce
Walidoux <ma.walidkorchi@icloud.com>
2022-12-05 00:55:20 +0100

    test(styles/config): adding fonts, adding extended theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: {
+          classe1: {
+            start: '#FFB72D',
+            end: '#FCAC12'
+          },
+          classe2: {
+            start: '#FFB72D',
+            end: '#FCAC12'
+          }
+        }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
