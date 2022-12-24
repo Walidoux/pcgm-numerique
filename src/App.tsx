0b309fdb13d91e402595d586ea34225733703aa2
Walidoux <ma.walidkorchi@icloud.com>
2022-12-24 20:46:47 +0100

    refactor(routes): update redirections

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { Classe } from './components/Classe/Classe'
-import { getClasse } from './data/Classes'
-import type { ClassesRange } from './types/Classe'
+import { Route, Routes } from 'react-router-dom'
+
+import { NavigationBar } from './components/NavigationBar'
+import { About, Home, Savings, Settings, Search } from './pages'
+import { routes } from './routes'
 
 const App: React.FC = () => {
+  const pageComponents = { About, Home, Savings, Settings, Search }
+
   return (
     <main className='m-3 max-w-[525px]'>
-      {Array.from({ length: 9 }, (_, i) => (
-        <Classe key={i} classe={getClasse((i + 1) as ClassesRange)} />
-      ))}
+      <Routes>
+        {routes.map((item, index) => {
+          const Currentcomponent = pageComponents[item.title]
+          return (
+            <Route
+              key={index}
+              path={item.redirect}
+              element={<Currentcomponent />}
+            />
+          )
+        })}
+      </Routes>
+
+      <NavigationBar />
     </main>
   )
 }
